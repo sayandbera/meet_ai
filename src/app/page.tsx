bf2d93e82683228755520f7bf238f15369612101
Sayan Bera <sayandbera@gmail.com>
2025-06-25 17:29:36 +0530

    Chapter 02 database setup done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Button } from "@/components/ui/button";
 import React from "react";
 
@@ -7,12 +5,7 @@ export default function Home() {
   return (
     <div className="text-4xl font-bold text-amber-300">
       Hello World!
-      <Button
-        variant="destructive"
-        onClick={() => console.log("Button has been clicked!")}
-      >
-        Click Me
-      </Button>
+      <Button variant="destructive">Click Me</Button>
     </div>
   );
 }
