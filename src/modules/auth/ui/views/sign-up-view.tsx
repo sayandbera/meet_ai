6f304f4a9bfe7a60693b372a3718e5963a3b9f41
Sayan Bera <sayandbera@gmail.com>
2025-06-27 18:06:52 +0530

    07 dashboard sidebar and navbar done

--- src/modules/auth/ui/views/sign-up-view.tsx
+++ src/modules/auth/ui/views/sign-up-view.tsx
@@ -251,7 +251,7 @@ export default function SignUpView() {
             </form>
           </Form>
 
-          <div className="bg-radial from-green-700 to-green-900 relative hidden md:flex flex-col gap-y-4 items-center justify-center">
+          <div className="bg-radial from-sidebar-accent to-sidebar relative hidden md:flex flex-col gap-y-4 items-center justify-center">
             <img
               src="/logo.svg"
               alt="Logo image"
--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,25 +1,7 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client";
-import { useRouter } from "next/navigation";
 import React from "react";
 
 export const HomeView = () => {
-  const router = useRouter();
-  return (
-    <Button
-      type="button"
-      variant="destructive"
-      onClick={() =>
-        authClient.signOut({
-          fetchOptions: {
-            onSuccess: () => router.push("/"),
-          },
-        })
-      }
-    >
-      Sign out
-    </Button>
-  );
+  return <div>Home page</div>;
 };
